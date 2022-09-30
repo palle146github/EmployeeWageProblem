add4ff9041a39b79a5f5234ed513c307578f707a
palle146github <112770521+palle146github@users.noreply.github.com>
2022-09-30 10:10:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_EmployeePresentOrNot.EmployeeAttendance();
+            // UC2_EmployeeDailyWage.EmployeeWage();
+            // UC3_EmpWagePartFullTime.EmpWagePartFullTime();
+            // UC4_EmpWageSwitchCase.EmpWageSwitchCase();
+            // UC5_EmpWageMaxWorkMonth.EmpWageMaxWorkMonth();
+            // UC6_EmpWageHourMaxDaysMonth.EmpWageHourMaxDaysMonth();
+            Console.ReadKey();
+        }
+    }
+}
