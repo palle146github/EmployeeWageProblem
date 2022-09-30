373f733bb6618d72e4ecf4df3c9fb504c719e46c
palle146github <112770521+palle146github@users.noreply.github.com>
2022-09-30 10:22:11 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageProblem
 {
-    class Program
+    class Program // this is the main program here all the other use cases are called
     {
         static void Main(string[] args)
         {
             UC1_EmployeePresentOrNot.EmployeeAttendance();
-            // UC2_EmployeeDailyWage.EmployeeWage();
-            // UC3_EmpWagePartFullTime.EmpWagePartFullTime();
-            // UC4_EmpWageSwitchCase.EmpWageSwitchCase();
-            // UC5_EmpWageMaxWorkMonth.EmpWageMaxWorkMonth();
-            // UC6_EmpWageHourMaxDaysMonth.EmpWageHourMaxDaysMonth();
+            UC2_EmployeeDailyWage.EmployeeWage();
+            UC3_EmpWagePartFullTime.EmpWagePartFullTime();
+            UC4_EmpWageSwitchCase.EmpWageSwitchCase();
+            UC5_EmpWageMaxWorkMonth.EmpWageMaxWorkMonth();
+            UC6_EmpWageHourMaxDaysMonth.EmpWageHourMaxDaysMonth();
             Console.ReadKey();
         }
     }
