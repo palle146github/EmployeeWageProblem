acb4e4c5dc484acb417d3a054d82e615f87c4f88
palle146github <112770521+palle146github@users.noreply.github.com>
2022-09-30 10:26:28 +0530

    Update UC1_EmployeePresentOrNot.cs

--- UC1_EmployeePresentOrNot.cs
+++ UC1_EmployeePresentOrNot.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageProblem
 {
-    public static class UC1_EmployeePresentOrNot
+    public static class UC1_EmployeePresentOrNot // this class is to check weather the employee present or not
     {
-        public static void EmployeeAttendance()
+        public static void EmployeeAttendance() // method declaration of use case 1
         {
             int IS_FULL_TIME = 1;
-            Random random = new Random();
-            int empcheck = random.Next(0, 2);
-            if (empcheck == IS_FULL_TIME)
+            Random random = new Random(); // random function to generate random numbers
+            int empcheck = random.Next(0, 2);// this will assign random number from 0 to 1
+            if (empcheck == IS_FULL_TIME)// checking condition for true
             {
                 Console.WriteLine("Employee is Present");
             }
