6f139ec1768b3c0af2922bb1ac7751120ef332e2
palle146github <112770521+palle146github@users.noreply.github.com>
2022-09-30 10:10:51 +0530

    Add files via upload

---
+++ UC1_EmployeePresentOrNot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public static class UC1_EmployeePresentOrNot
+    {
+        public static void EmployeeAttendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
